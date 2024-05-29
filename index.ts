66398bc032e9a4364167779c2a549e4e0660fbc4
Simple Guruji <dashingak957014@gmail.com>
2024-05-29 16:14:57 +0530

    setup express

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+import app from "./src/app";
+import { config } from "./src/config/config";
 import dbConnect from "./src/config/db";
 
-dbConnect();
+dbConnect()
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`Server running on port ${config.port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(
+      `Error: ${err}. Please check your database connection settings in the config/db.js file!`
+    );
+  });
